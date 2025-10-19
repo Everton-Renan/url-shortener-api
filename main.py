6990d90aa4557d0e12fc67b1438372ac90a2d7c7
Everton-Renan <evertonrenan917@gmail.com>
2025-10-19 18:36:34 -0300

    Add FastAPI endpoints to create and access shortened URLs

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from generate_short_url import generate_url
+
+app = FastAPI()
+urls: dict[str, str] = dict()
+
+
+@app.get("/create")
+def create(url: str):
+    if url is None or url is False:
+        return {"error": "Parameter URL not sent."}
+
+    short_url = generate_url(4)
+    urls[short_url] = url
+    return {"short-url": short_url}
+
+
+@app.get("/access/{short_url}")
+def access(short_url: str):
+    for k, v in urls.items():
+        if k == short_url:
+            return RedirectResponse(v)
+    return {"error": "Url not found."}
