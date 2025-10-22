45f2003012097103967aa21811fc6faccb52e4ed
Everton-Renan <evertonrenan917@gmail.com>
2025-10-22 00:21:23 -0300

    Remove "access" (view), add validations to check if the submitted url is empty or if it has "error" in result.lower()

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.exceptions import HTTPException
 
-from generate_short_url import generate_url
+from database import create_url
 
 app = FastAPI()
-urls: dict[str, str] = dict()
 
 
 @app.post("/shorten")
 def create(url: str):
-    if url is None or url is False:
-        return {"error": "Parameter URL not sent."}
+    if not url:
+        raise HTTPException(status_code=400, detail="Parameter URL not sent.")
 
-    short_url = generate_url(4)
-    urls[short_url] = url
-    return {"short-url": short_url}
+    result = create_url(url)
 
-
-@app.get("/{short_url}")
-def access(short_url: str):
-    for k, v in urls.items():
-        if k == short_url:
-            return RedirectResponse(v)
-    return {"error": "Url not found."}
+    if "error" in result.lower():
+        raise HTTPException(status_code=500, detail=result)
+    return {"short-url": result}
