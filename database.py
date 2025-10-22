9ac578e77db4d2e7bcc1c91d5239977639aa8aa8
Everton-Renan <evertonrenan917@gmail.com>
2025-10-22 18:11:25 -0300

    Rename "original" to "original_url" and create "get_url" (function)

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.orm.session import sessionmaker
 
@@ -5,12 +6,12 @@ from generate_short_url import generate_url
 from init_db import Urls, engine
 
 
-def create_url(original: str):
+def create_url(original_url: str):
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
         short_url = generate_url(4)
-        url = Urls(original=original, short_url=short_url)
+        url = Urls(original_url=original_url, short_url=short_url)
         session.add(url)
         session.commit()
     except IntegrityError as e:
@@ -23,3 +24,14 @@ def create_url(original: str):
         session.close()
 
     return short_url
+
+
+def get_url(short_url: str):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = select(Urls).where(Urls.short_url == short_url)
+    url = session.scalars(query).first()
+    if url is not None:
+        return url.original_url
+    return None
