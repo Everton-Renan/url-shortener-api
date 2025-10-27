d01cd2db8f7ab90382f254b7b307d24f5a73b124
Everton-Renan <evertonrenan917@gmail.com>
2025-10-26 22:02:07 -0300

    Add a validation to check if expires_at is less than the current date

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import RedirectResponse
-
 from database import create_url, get_url, update_clicks
-
+from datetime import datetime
 app = FastAPI()
 
 
@@ -21,8 +20,14 @@ def create(url: str):
 
 @app.get("/{short_url}")
 def access(short_url: str):
-    original_url = get_url(short_url)
-    if original_url is not None:
-        update_clicks(short_url, 1)
-        return RedirectResponse(str(original_url))
+    url = get_url(short_url)
+
+    if url is not None:
+        date = datetime.now()
+        original_url = url[0]
+        expires_at = datetime.strptime(str(url[1]), "%Y-%m-%d %H:%M:%S.%f")
+
+        if date < expires_at:
+            update_clicks(short_url, 1)
+            return RedirectResponse(str(original_url))
     return {"error": "Url not found."}
