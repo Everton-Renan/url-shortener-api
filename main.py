a2917abc41795e27c09541c92cdd49b4c4c1c096
Everton-Renan <evertonrenan917@gmail.com>
2025-10-27 21:37:59 -0300

    Change the "access" endpoint to "/get/{short_url}" and add new validations, error messages, and responses

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 
 from database import create_url, get_url, get_urls, update_clicks
 
@@ -17,30 +17,46 @@ def create(url: str):
 
     result = create_url(url)
 
-    if "error" in result.lower():
-        raise HTTPException(status_code=500, detail=result)
-    return {"short-url": result}
+    if isinstance(result, Exception):
+        raise HTTPException(
+            status_code=500,
+            detail="There was an error with the database, please try again later.",
+        )
+    return JSONResponse({"short-url": result}, 201)
 
 
-@app.get("/create/{short_url}")
+@app.get("/get/{short_url}")
 def access(short_url: str):
     url = get_url(short_url)
 
     if url is not None:
+        if isinstance(url, Exception):
+            raise HTTPException(
+                status_code=500,
+                detail="There was an error with the database, please try again later.",
+            )
+
         date = datetime.now()
         original_url = url[0]
         expires_at = datetime.strptime(str(url[1]), "%Y-%m-%d %H:%M:%S.%f")
 
         if date < expires_at:
             update_clicks(short_url, 1)
-            return RedirectResponse(str(original_url))
-    return {"error": "Url not found."}
+            return RedirectResponse(str(original_url), 302)
+    return JSONResponse({"error": "Url not found."}, 404)
 
 
 @app.get("/urls")
 def urls():
     urls = get_urls()
+
     if urls is not None:
+        if isinstance(urls, Exception):
+            raise HTTPException(
+                status_code=500,
+                detail="There was an error with the database, please try again later.",
+            )
+
         json_urls = jsonable_encoder(urls)
-        return json_urls
-    return {"error": "URLs not found."}
+        return JSONResponse({"urls": json_urls}, 200)
+    return JSONResponse({"error": "URLs not found."}, 404)
